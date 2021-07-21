7dbd15bbf9959e9a68c3eff3f57a4dcfad02c0d2
Romain Goulet <romain.goulet@edu.devinci.fr>
2021-07-21 15:57:31 +0200

    add fb

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
     signInFlow: "popup",
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
     callbacks: {
